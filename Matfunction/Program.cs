b63a6fa551b3aa4073e9f03947b176c5b7b304dc
Alice <algus_91@mail.rugit config --global user.email algus_91@mail.ru>
2022-08-17 18:11:59 +0300

    matfunction

---
+++ Matfunction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Matfunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Abs(number));
+            Console.WriteLine(Math.Sqrt(number));
+            Console.WriteLine(Math.Round(Math.PI,3));
+        }
+    }
+}
